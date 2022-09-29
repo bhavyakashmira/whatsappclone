6ff9618b7d27565c9528b54fe4ce762734503de1
bhavya kashmira <bhavyakashmira@gmail.com>
2022-09-29 21:28:02 +0530

    Dynamic Widget

--- lib/chatpage.dart
+++ lib/chatpage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/CustomUI/CustomCard.dart';
+import 'package:whatsapp/Model/ChatModel.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -9,18 +10,43 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  List<ChatModel> chats = [
+    ChatModel(
+      name: "IEEE",
+      isGroup : false,
+      currentMessage: "hi everyone",
+      time: "4:00",
+      icon : "person.svg"
+    ),
+        ChatModel(
+      name: "Bhavya",
+      isGroup : false,
+      currentMessage: "hi everyone",
+      time: "4:00",
+      icon : "person.svg"
+    ),
+        ChatModel(
+      name: "AAYUSH",
+      isGroup : false,
+      currentMessage: "hi everyone",
+      time: "4:00",
+      icon : "person.svg"
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-      child: const Icon(Icons.chat),
+        child: const Icon(Icons.chat),
       ),
-     body: ListView(
-      children: [
-        CustomCard(),
-      ],
-     ), );
+      body: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (context, index) => CustomCard(
+          chatModel: chats[index],
+        ),
+        ),
+         );
     
   }
 }
---
+++ lib/screens/IndividualPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IndividualPage extends StatefulWidget {
+  const IndividualPage({Key? key}) : super(key: key);
+
+  @override
+  State<IndividualPage> createState() => _IndividualPageState();
+}
+
+class _IndividualPageState extends State<IndividualPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
